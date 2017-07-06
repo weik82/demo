78b005fbd11b0564f242386fdf3156800d341c71
weik82 <weikang82@foxmail.com>
2017-07-06 11:12:06 +0800

    xhr

--- demo/xhr/xhr.js
+++ demo/xhr/xhr.js
@@ -4,7 +4,7 @@ function isEmptyObject(e) {
     return true;
 }
 function parse2query(object) {
-    let str = '';
+    let str;
     if (!isEmptyObject(object)) {
         str = JSON.stringify(object);
         return '?' + str.replace(/[\"{}]/g, '').replace(/,/g, '&').replace(/:/g, '=');
@@ -16,14 +16,18 @@ function parseDate(xhr, resolve, reject) {
     return function () {
         if (xhr.readyState === 4) {
             if (xhr.status >= 200 && (xhr.status < 300 || xhr.status === 304)) {
-                let _data, _header;
+                let _data, _header, _headerMap = {};
                 if (xhr.responseText) {
                     _data = JSON.parse(xhr.responseText);
                 } else {
                     _data = xhr.responseText;
                 }
                 _header = xhr.getAllResponseHeaders();
-                resolve({data: _data, headers: _header})
+                _header.split(/\r\n/).forEach((item) => {
+                    let _map = item.split(':');
+                    _map.length > 1 && (_headerMap[_map[0]] = _map[1]);
+                });
+                resolve({data: _data, headers: _headerMap})
             } else {
                 reject(`XHR error:${xhr.status}`);
             }
@@ -41,29 +45,37 @@ function setHeaders(xhr, headers) {
         }
     }
 }
-console.log(parse2query({id: 1, name: 'kk'}));
 class XHR {
-    get(url, data = {}, headers = {}) {
+    constructor() {
+        this.xhr = new XMLHttpRequest();
+        this.commonHeaders = {};
+    }
+
+    setCommonHeader(headers = {}) {
+        this.commonHeaders = headers;
+    }
+
+    $http(method, url, data = {}, headers = {}) {
         return new Promise((resolve, reject) => {
-            const xhr = new XMLHttpRequest();
+            const xhr = this.xhr;
+            let _method = method.toLowerCase();
             xhr.onreadystatechange = parseDate(xhr, resolve, reject);
-            url += parse2query(data);
-            xhr.open('get', url, true);
+            if (_method === 'get') {
+                url += parse2query(data);
+            }
+            xhr.open(method, url, true);
             xhr.setRequestHeader('content-type', 'application/json');
-            setHeaders(xhr, headers);
-            xhr.send(null);
+            setHeaders(xhr, Object.assign({}, this.commonHeaders, headers));
+            xhr.send((_method === 'get' ? null : JSON.stringify(data)));
         });
     }
 
+    get(url, data = {}, headers = {}) {
+        return this.$http('get', url, data, headers);
+    }
+
     post(url, data, headers = {}) {
-        return new Promise((resolve, reject) => {
-            const xhr = new XMLHttpRequest();
-            xhr.onreadystatechange = parseDate(xhr, resolve, reject);
-            xhr.open('post', url, true);
-            xhr.setRequestHeader('content-type', 'application/json');
-            setHeaders(xhr, headers);
-            xhr.send(JSON.stringify(data));
-        });
+        return this.$http('post', url, data, headers);
     }
 }
 
@@ -74,10 +86,19 @@ class XHR {
  };
 
  export default XHR;*/
-const ajax = new XHR();
-ajax.post('http://10.12.10.20:8080/CACoreV2/v2/user/loginapi', {
-    "username": "bj@admin",
-    "userpassword": "c4ca4238a0b923820dcc509a6f75849b"
+const api = new XHR();
+api.setCommonHeader({
+    "CA-Token": '3413]39DD6B48F926B7810001015D15AFB287]1]5]1]1',
+    "Client-Flag": 1
+});
+api.post('http://10.12.10.20:8080/CACoreV2/v2/user/getusertreeapi', {
+    "roleid": 1,
+    "userid": 1,
+    "usertype": 5,
+    "nusertype": 0,
+    "companyid": 2,
+    "aaid": "-1",
+    "ppid": "1"
 }).then(function (res) {
-    console.log(res);
+    console.dir(res);
 });
